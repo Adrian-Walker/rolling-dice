283c5c486ef58bdd3125ceb69698765b1cf62b0b
Adrian Walker <awalker3365@gmail.com>
2021-08-09 12:44:32 -0400

    8-9-21 1244p

---
+++ Dicebox.js
@@ -0,0 +1,14 @@
+import React from "react"
+import './index.css';
+
+
+
+function Dicebox() {
+    return (
+        <div className="dicebox">
+
+        </div>
+    );
+}
+
+export default Dicebox;
