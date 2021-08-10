fef7e60c060137e8a3cda691b5fb429099756b44
Adrian Walker <awalker3365@gmail.com>
2021-08-10 13:55:05 -0400

    8-10-21 155p

--- Dicebox.js
+++ Dicebox.js
@@ -1,14 +1,30 @@
+import { render } from "@testing-library/react";
 import React from "react"
 import './index.css';
 
+class Dicebox extends React.Component {
+    constructor() {
+        super()
+        this.state = {
 
+        }
+    }
+    render() {
+        return (
+            <div className="dicebox">
+                <ul className="die">
+                    <li className="dice1"><span className="dot1"></span></li>
+                    <li className="dice2"><span className="dot2"></span></li>
+                    <li className="dice3"><span className="dot3"></span></li>
+                    <li className="dice4"><span className="dot4"></span></li>
+                    <li className="dice5"><span className="dot5"></span></li>
+                </ul>
+            </div>
+        );
+    }
 
-function Dicebox() {
-    return (
-        <div className="dicebox">
-
-        </div>
-    );
 }
+
+
 
 export default Dicebox;
