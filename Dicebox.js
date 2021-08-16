11384980c6e817b144ead1621a361e465047e192
Adrian Walker <awalker3365@gmail.com>
2021-08-15 23:02:17 -0400

    8-15-21 1102p

--- Dicebox.js
+++ Dicebox.js
@@ -1,9 +1,4 @@
-import { render } from "@testing-library/react";
-import React from "react"
-import './index.css';
 
-
-function Dicebox() {
     <div>
         <button>Press Me</button>
     </div>
