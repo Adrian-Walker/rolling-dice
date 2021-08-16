b7e821b69286d379df00ee7372e01e13f336d356
Adrian Walker <awalker3365@gmail.com>
2021-08-15 23:01:38 -0400

    8-15-21 1101p

--- App.js
+++ App.js
@@ -1,10 +1,25 @@
-import React from "react"
+import React, { Component } from "react"
+import Dicebox from "./Dicebox";
 import './index.css';
+// import Dicebox from "./Dicebox";
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      dice1: 0,
+      dice2: 0,
+      dice3: 0,
+      dice4: 0,
+      dice5: 0
+    }
+  }
+}
 
 function App() {
   return (
     <div className="App">
-
+      <Dicebox />
     </div>
   );
 }
--- Dicebox.js
+++ Dicebox.js
@@ -2,29 +2,12 @@ import { render } from "@testing-library/react";
 import React from "react"
 import './index.css';
 
-class Dicebox extends React.Component {
-    constructor() {
-        super()
-        this.state = {
 
-        }
-    }
-    render() {
-        return (
-            <div className="dicebox">
-                <ul className="die">
-                    <li className="dice1"><span className="dot1"></span></li>
-                    <li className="dice2"><span className="dot2"></span></li>
-                    <li className="dice3"><span className="dot3"></span></li>
-                    <li className="dice4"><span className="dot4"></span></li>
-                    <li className="dice5"><span className="dot5"></span></li>
-                </ul>
-            </div>
-        );
-    }
-
+function Dicebox() {
+    <div>
+        <button>Press Me</button>
+    </div>
 }
-
 
 
 export default Dicebox;
