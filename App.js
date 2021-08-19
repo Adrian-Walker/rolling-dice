11f07884cb0c1a757f5cbe9ed0f1efc02adc68dc
Adrian Walker <awalker3365@gmail.com>
2021-08-19 18:44:29 -0400

    8-19-21 644p

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react"
+import { render } from "react-dom";
 import Dicebox from "./Dicebox";
 import './index.css';
 // import Dicebox from "./Dicebox";
@@ -7,21 +8,26 @@ class App extends Component {
   constructor() {
     super()
     this.state = {
-      dice1: 0,
-      dice2: 0,
-      dice3: 0,
-      dice4: 0,
-      dice5: 0
+      dice1: "/Users/adrianwalker/V-School/Level-3/rolling-dice/src/dice/dice-1.png",
+      dice2: "/Users/adrianwalker/V-School/Level-3/rolling-dice/src/dice/dice-2.jpeg",
+      dice3: "/Users/adrianwalker/V-School/Level-3/rolling-dice/src/dice/dice-3.png",
+      dice4: "/Users/adrianwalker/V-School/Level-3/rolling-dice/src/dice/dice-4.png",
+      dice5: "/Users/adrianwalker/V-School/Level-3/rolling-dice/src/dice/dice-5.png",
+      dice6: "/Users/adrianwalker/V-School/Level-3/rolling-dice/src/dice/dice-6.png"
     }
+  }
+
+  render() {
+
+    return (
+      <div className="App">
+        <Dicebox />
+      </div>
+    )
+
   }
 }
 
-function App() {
-  return (
-    <div className="App">
-      <Dicebox />
-    </div>
-  );
-}
+
 
 export default App;
--- index.js
+++ index.js
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-import Dicebox from './Dicebox';
+
+import App from './App';
 
 
 
 
-ReactDOM.render(<Dicebox />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
